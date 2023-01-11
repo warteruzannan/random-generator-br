a58f25d5dc070a7b581e496d77659d745147358a
Warteruzannan S. Cunha <warteruzannan@gmail.com>
2023-01-11 18:26:11 -0300

    refactor: isolating textInput

---
+++ src/presentation/components/input/index.tsx
@@ -0,0 +1,39 @@
+import {memo} from 'react';
+import {StyleSheet, TextInput, TextInputProps, View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {colors} from '../../consts';
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    color: '#444',
+    fontSize: 21,
+    flex: 1,
+    fontWeight: 'bold',
+    marginLeft: 10,
+  },
+});
+
+type InputProps = {
+  icon?: string;
+} & TextInputProps;
+
+const Input: React.FC<InputProps> = props => {
+  const {icon} = props;
+  return (
+    <View style={styles.container}>
+      {icon && <Icon name={'lock'} size={30} color={colors.TRANSPARENT} />}
+      <TextInput
+        {...props}
+        underlineColorAndroid={'#999'}
+        textAlign="left"
+        style={styles.textInput}
+      />
+    </View>
+  );
+};
+
+export default memo(Input);
--- src/presentation/screens/password/index.tsx
+++ src/presentation/screens/password/index.tsx
@@ -1,36 +1,16 @@
-import {memo, useEffect, useState} from 'react';
-import {TextInput, View, ViewProps} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome5';
-import {colors} from '../../consts';
+import {memo, useState} from 'react';
+import {View, ViewProps} from 'react-native';
 import Clipboard from '@react-native-clipboard/clipboard';
 import Button from '../../components/button';
 import {generatePassword} from '../../../core/usecases/generate-password';
+import Input from '../../components/input';
 
 const Password: React.FC<ViewProps> = props => {
-  const [password, setPassword] = useState<string>('035.976.121-64');
-
-  useEffect(() => {
-    setPassword(generatePassword());
-  }, []);
+  const [password, setPassword] = useState<string>(generatePassword());
 
   return (
     <View {...props} style={{flex: 1, padding: 10, alignItems: 'center'}}>
-      <View style={{flexDirection: 'row', alignItems: 'center'}}>
-        <Icon name={'lock'} size={30} color={colors.TRANSPARENT} />
-        <TextInput
-          underlineColorAndroid={'#999'}
-          textAlign="left"
-          value={password}
-          editable={false}
-          style={{
-            color: '#444',
-            fontSize: 21,
-            flex: 1,
-            fontWeight: 'bold',
-            marginLeft: 10,
-          }}
-        />
-      </View>
+      <Input icon="lock" value={password} editable={false} />
 
       <Button
         icon="redo"
